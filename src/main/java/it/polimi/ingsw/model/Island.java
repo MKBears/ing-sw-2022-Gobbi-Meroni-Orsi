c72444242f560f1aa19ea5867124db094bd40016
Marco Orsi <100961566+MKBears@users.noreply.github.com>
2022-03-30 12:38:17 +0200

    Add Island

---
+++ src/main/java/it/polimi/ingsw/model/Island.java
@@ -0,0 +1,105 @@
+package it.polimi.ingsw.model;
+
+import java.util.ArrayList;
+
+public class Island {
+    private ArrayList<Student> students;
+    private final short islandID;
+    private Tower tower;
+    private boolean noEntry;
+
+    public Island (short id){
+        islandID = id;
+        tower = null;
+        noEntry = false;
+    }
+
+    public short getIslandID() {
+        return islandID;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public ArrayList<Student> getStudents(){
+        ArrayList<Student> temp = students;
+        students = null;
+        return temp;
+    }
+
+    public Color getControllingColor() throws NoControlException{
+        if (tower == null){
+            throw new NoControlException();
+        }
+        return tower.getColor();
+    }
+
+    public Type_Student getControllingType() {
+        short[] counter = new short[5];
+        short max = 0;
+
+        if (students.isEmpty()) {
+            return null;
+        } else {
+            for (short i = 0; i < 5; i++) {
+                counter[i] = 0;
+            }
+
+            for (Student s : students) {
+                switch (s.getType().getName()) {
+                    case "dragon":
+                        counter[0]++;
+                        break;
+                    case "gnome":
+                        counter[1]++;
+                        break;
+                    case "fairie":
+                        counter[2]++;
+                        break;
+                    case "unicorn":
+                        counter[3]++;
+                        break;
+                    default:
+                        counter[4]++;
+                        break;
+                }
+            }
+
+            for (short i = 1; i < 5; i++) {
+                if (counter[i] > counter[max]) {
+                    max = i;
+                }
+            }
+
+            switch (max) {
+                case 0:
+                    return Type_Student.DRAGON;
+                case 1:
+                    return Type_Student.GNOME;
+                case 2:
+                    return Type_Student.FAIRIE;
+                case 3:
+                    return Type_Student.UNICORN;
+                default:
+                    return Type_Student.FROG;
+            }
+        }
+    }
+
+    public boolean isThereNoEntry(){
+        return noEntry;
+    }
+
+    public void setNoEntry(boolean noEntry) throws DuplicateValueException{
+        if (noEntry == this.noEntry){
+            throw new DuplicateValueException();
+        }
+        this.noEntry = noEntry;
+    }
+
+    public void setTower(Tower tower) {
+        this.tower = tower;
+    }
+
+}
