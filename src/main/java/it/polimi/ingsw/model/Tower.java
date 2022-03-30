6669cbe145bc166dcdd63770bbf2bae97a744596
Marco Orsi <100961566+MKBears@users.noreply.github.com>
2022-03-30 12:33:56 +0200

    Add Tower

---
+++ src/main/java/it/polimi/ingsw/model/Tower.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.model;
+
+public class Tower {
+    private final Color color;
+
+    public Tower (Color color){
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+}
